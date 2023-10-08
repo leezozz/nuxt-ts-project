7b10a6214a94bbbf4b080df1a3f128c34ba66695
leezozz <157297759@qq.com>
2023-10-08 17:28:40 +0800

    fix: modify title describe

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="nuxt-default-layout">
-    <slot></slot>
+  <div>
+    test===
   </div>
 </template>
 
 <script setup lang="ts">
+useSeoMeta({
+  title: '测试页'
+})
 
+console.log('测试页')
 </script>
 
 <style scoped lang="scss">
-.nuxt-default-layout {
-  padding: 20px;
-}
+
 </style>
