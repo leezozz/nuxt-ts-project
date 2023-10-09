fef794abf43ee8e091f9b1610af4f0affc83f754
gaofuhong <157297759@qq.com>
2023-10-09 18:02:15 +0800

    feat: add dynamic route

---
+++ src/pages/test/people/[peopleId].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    测试页-peopleId
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: '测试页-peopleId'
+})
+console.log('测试页-peopleId')
+
+const route = useRoute()
+console.log('测试页-peopleId-route', route, route.params)
+</script>
+
+<style scoped lang="scss">
+
+</style>
